4d9de07f8e4c11928e7ea2732320350c3adcc29c
akmal <akmal@brighter.com>
2014-09-29 18:31:06 -0700

    animate and neighbors on detail

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-angular.module("cncApp", ['ngRoute']).
+var app = angular.module("cncApp", ['ngRoute','ngAnimate']).
 config(function($routeProvider){
         $routeProvider.when('/', {
             templateUrl : './home.html',
@@ -7,21 +7,22 @@ config(function($routeProvider){
             templateUrl : './countries.html',
             controller : 'countriesController',
             resolve: {
-            	countries : function(countriesService){
-            		var countries = [];
-            		countriesService.countries().then(function(data){
-            			angular.forEach(data.geonames, function(c){
-            				countries.push(c);
-            			});
-            		});
-
-            		return countries;
+            	countries : function(geoNamesService){
+                    return geoNamesService.get('countryInfo',{});
             	}
             }
         })
         .when('/countries/:countryCode/capital',{
             templateUrl : './countryDetail.html',
-            controller: 'CountryDetailController'
+            controller: 'CountryDetailController',
+            resolve: {
+                countryInfo : function($route, geoNamesService) {
+                    return geoNamesService.get('countryInfo',{country: $route.current.params.countryCode});
+                },
+                neighbours : function($route, geoNamesService) {
+                    return geoNamesService.get('neighbours',{country: $route.current.params.countryCode});
+                }
+            }
         });
 
     }).
@@ -31,22 +32,41 @@ config(function($routeProvider){
 	controller("countriesController", function($scope, countries){
 	$scope.title = "Countries n Capitals";
 	
-	$scope.countries = countries;
+	$scope.countries = countries.geonames;
 	
-}).controller('CountryDetailController', ['$scope','$routeParams', function($scope,$routeParams){
-    $scope.countryCode = $routeParams.countryCode
+}).controller('CountryDetailController', ['$scope','countryInfo','neighbours', function($scope,countryInfo, neighbours){
+   $scope.country = countryInfo.geonames[0];
+   $scope.neighbours = neighbours.geonames;
 }])
-    .service("countriesService", function($http, $q){
+    .service("geoNamesService", function($http, $q){
 	return {
-		countries : function(){
-			var params = {username: 'akmal_muqeeth', type: 'JSON'};
-			return $http.get("http://api.geonames.org/countryInfo",{params: params}).
-			then(function(response){
-				return response.data;
-				
-			}, function(response){
-				return $q.reject(response.data);
-			});
-		}
+
+        get : function(path, params){
+
+            var base_url = "http://api.geonames.org/";
+            params = angular.extend({username: 'akmal_muqeeth', type: 'JSON'}, params || {})
+
+            var defer = $q.defer();
+            $http.get(base_url + path, {params : params, cache:true}).
+            success(function(response){
+                defer.resolve(response);
+            });
+
+            return defer.promise;
+        }
 	};
 });
+
+app.run(['$rootScope',function($rootScope){
+    
+    $rootScope.$on('$routeChangeStart', function(){
+        $rootScope.isLoading =true;
+        console.log("route change start");
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function(){
+        $rootScope.isLoading =false;
+        console.log("route change success");
+    });
+
+}]);
