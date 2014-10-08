dab72c1d88921a05518a79ce980c96fbd1b455c7
akmal <akmal@brighter.com>
2014-10-07 17:23:30 -0700

    unit test for geo service

---
+++ test/appSpec.js
@@ -0,0 +1,21 @@
+describe("geoNamesService", function(){
+	beforeEach(module('cncApp'));
+
+	it('should query the back end when a resource is fetched ', function() {
+		inject(function(geoNamesService, $rootScope, $httpBackend) {
+			//Arrange- mock the http request
+        	$httpBackend.expect('GET', 'http://api.geonames.org/path?name=name&type=JSON&username=akmal_muqeeth').respond(200);
+        	
+        	var status = false;
+        	// Act
+        	geoNamesService.get("path", {name: 'name'}).then(function(){
+        		status = true;
+        	});
+        	$rootScope.$digest();
+        	$httpBackend.flush();
+        	// Assert
+        	expect(status).toBe(true);
+        	$httpBackend.verifyNoOutstandingRequest();
+        });
+	});
+});
